30839f5d435fba15089a70010b96d9c3245ce79e
abmcompanysn-dot <abmcompanysn@gmail.com>
2025-09-19 17:08:25 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+// ==================================================================
+// --- FICHIER DE CONFIGURATION CENTRALISÉ ---
+// Modifiez l'URL de l'API ici. Ce fichier est partagé par toutes les pages.
+// ==================================================================
+
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbysStq480_wKltbsJiYdKbQZxgfXZ-XMFkGmaWr8bQW6GYxQ7atbBLKwiqAckgtHztwqQ/exec';
+
+// --- Clés de Session (partagées) ---
+const ADMIN_SESSION_KEY = 'abm_admin_session';
+const STUDENT_SESSION_KEY = 'abm_student_session';
+const RESPONSABLE_SESSION_KEY = 'abm_responsable_session';
