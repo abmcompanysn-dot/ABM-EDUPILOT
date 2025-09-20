9c4b74556ee4294ffb848119ed1ce457e2af5c50
abmcompanysn-dot <abmcompanysn@gmail.com>
2025-09-20 07:01:00 +0000

    Update config.js

--- config.js
+++ config.js
@@ -3,11 +3,12 @@
 // Modifiez l'URL de l'API ici. Ce fichier est partagé par toutes les pages.
 // ==================================================================
 
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxfRm7Z8pc0D83u7njZxCtaW47m54AF2TQoIlG_FqVdph2Upql7qj5Oa1W_Pf6GdneNFw/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbz4TjtAAujMDWIyUxYGpLJfKp2w6--NltzPIEbfwUnktwCqv8JhN3bl3-iT-L4mll91-w/exec';
 
 // --- Clés de Session (partagées) ---
 const ADMIN_SESSION_KEY = 'abm_admin_session';
 const STUDENT_SESSION_KEY = 'abm_student_session';
 const RESPONSABLE_SESSION_KEY = 'abm_responsable_session';
+
 
 
