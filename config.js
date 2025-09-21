c2ba9610292b93f96924ef5ba775f882ad27f7f8
abmcompanysn-dot <abmcompanysn@gmail.com>
2025-09-21 21:47:55 +0000

    Update config.js

--- config.js
+++ config.js
@@ -3,17 +3,12 @@
 // Modifiez l'URL de l'API ici. Ce fichier est partagé par toutes les pages.
 // ==================================================================
 
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxZXvruZ_RWwcaGo2C5FjAQbQFX2FoX8AaU353BsgvJfG7b7QfkvUbibO5fmRmOxuV4ug/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby1sek7IxNKsXQGnqR5dDPLICWUof707VQqqGKD1TYq-fa2a-TInJlskWWv5eA9axonkw/exec';
+
+// NOUVEAU: Client ID pour l'authentification Google. Remplacez par votre propre ID.
+const GOOGLE_CLIENT_ID = "861525588418-llkf68sghrjta6aqghrs4map3hdl74g5.apps.googleusercontent.com";
 
 // --- Clés de Session (partagées) ---
 const ADMIN_SESSION_KEY = 'abm_admin_session';
 const STUDENT_SESSION_KEY = 'abm_student_session';
 const RESPONSABLE_SESSION_KEY = 'abm_responsable_session';
-
-
-
-
-
-
-
-
