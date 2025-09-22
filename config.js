39f7418c965f422b47ac159c1d0611c0b7c74a6d
abmcompanysn-dot <abmcompanysn@gmail.com>
2025-09-22 19:11:35 +0000

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@
 // 2. Cliquez sur "Déployer" > "Gérer les déploiements".
 // 3. Assurez-vous que votre déploiement est de type "Application Web".
 // 4. Copiez l'URL de l'application Web et collez-la ici.
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxh9jtKuNTuQC-coE74nn5vKUDlAV8J9n_R7K6LwKlNHXe14acqIwUedBwxCeiq1ZZQkw/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby1xMnIuuaYXJOytfnvTVcMxjE7PD4LcGSZucZr-4cBFqsAgVT9-NsigID9bXyjox7h8A/exec';
 // CORRECTION: Utilisation des noms de clés de session cohérents avec les autres fichiers.
 // --- Clés de session et autres constantes ---
 const ADMIN_SESSION_KEY = 'abm_admin_token';
@@ -27,3 +27,4 @@ const PAGE_URLS = {
 
 // --- ID Client Google (doit correspondre à celui du backend) ---
 const GOOGLE_CLIENT_ID = "861525588418-llkf68sghrjta6aqghrs4map3hdl74g5.apps.googleusercontent.com";
+
