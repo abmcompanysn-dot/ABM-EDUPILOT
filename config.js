a92d31fd59779214a4ac229ce9cb97f496d00dc2
abmcompanysn-dot <abmcompanysn@gmail.com>
2025-09-22 23:08:48 +0000

    Update config.js

--- config.js
+++ config.js
@@ -2,15 +2,8 @@
 /*               FICHIER DE CONFIGURATION DU FRONTEND                 */
 /* ================================================================== */
 
-// --- IMPORTANT ---
-// Remplacez la valeur ci-dessous par l'URL de déploiement de votre
-// script Google Apps Script.
-// Pour l'obtenir :
-// 1. Ouvrez votre projet Apps Script.
-// 2. Cliquez sur "Déployer" > "Gérer les déploiements".
-// 3. Assurez-vous que votre déploiement est de type "Application Web".
-// 4. Copiez l'URL de l'application Web et collez-la ici.
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby1xMnIuuaYXJOytfnvTVcMxjE7PD4LcGSZucZr-4cBFqsAgVT9-NsigID9bXyjox7h8A/exec';
+
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwsgssxLFDWnoUsvF3wFiPfzEGxjYCPCyqluVfsZ2ZoOBBhu_7kRJmir9goaahsQIgR/exec';
 // CORRECTION: Utilisation des noms de clés de session cohérents avec les autres fichiers.
 // --- Clés de session et autres constantes ---
 const ADMIN_SESSION_KEY = 'abm_admin_token';
@@ -25,6 +18,4 @@ const PAGE_URLS = {
     RESPONSABLE: 'responsable.html'
 };
 
-// --- ID Client Google (doit correspondre à celui du backend) ---
-const GOOGLE_CLIENT_ID = "861525588418-llkf68sghrjta6aqghrs4map3hdl74g5.apps.googleusercontent.com";
 
