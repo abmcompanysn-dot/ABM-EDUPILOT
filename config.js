8df775b10eb553ba2d17c6819629a9fe9f58c999
abmcompanysn-dot <abmcompanysn@gmail.com>
2025-09-22 14:01:47 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,20 @@
 // ==================================================================
-// --- FICHIER DE CONFIGURATION CENTRALISÉ ---
-// Modifiez l'URL de l'API ici. Ce fichier est partagé par toutes les pages.
+// FICHIER DE CONFIGURATION CENTRALISÉ
+// Ce fichier contient toutes les constantes partagées par l'application.
 // ==================================================================
 
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzmnsQQREVjxFwoK670j7PKH1YAmX_661LxDMJS1xwp2Dw-sYAL-Xsdx2rJLpAzcdf5hA/exec';
-
-// NOUVEAU: Client ID pour l'authentification Google. Remplacez par votre propre ID.
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby1sek7IxNKsXQGnqR5dDPLICWUof707VQqqGKD1TYq-fa2a-TInJlskWWv5eA9axonkw/exec';
 const GOOGLE_CLIENT_ID = "861525588418-llkf68sghrjta6aqghrs4map3hdl74g5.apps.googleusercontent.com";
 
-// --- Clés de Session (partagées) ---
+// Clés de Session (partagées)
 const ADMIN_SESSION_KEY = 'abm_admin_session';
 const STUDENT_SESSION_KEY = 'abm_student_session';
 const RESPONSABLE_SESSION_KEY = 'abm_responsable_session';
 
-
+// NOUVEAU: URLs des pages pour une maintenance facile
+const PAGE_URLS = {
+    HOME: 'index.html',
+    ADMIN: 'admin.html',
+    RESPONSABLE: 'responsable.html',
+    STUDENT: 'etudiant.html'
+};
