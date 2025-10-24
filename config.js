5e7b5fff1b227a9c6225cae6f844442db4148938
abmcompanysn-dot <abmcompanysn@gmail.com>
2025-10-24 15:41:33 +0000

    Ajout des modifications sur junior-senior

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 /* ================================================================== */
 
 
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxH3HzXakRChNE0n1zlm9Rl6CFY0m1HrLgEVwIHmY88ILVze8CXJz8DtNwjFdIOHijCnA/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzwwUQxyuVmDzHERuO4efYFyhbPdjWw1MvuKamjs70Yt9kWDQfJWByMKloM_vycPKMFqA/exec';
 // CORRECTION: Utilisation des noms de clés de session cohérents avec les autres fichiers.
 // --- Clés de session et autres constantes ---
 const ADMIN_SESSION_KEY = 'abm_admin_token';
