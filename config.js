34255f1edacad8c9e4b9605dc6280f84f147f310
abmcompanysn-dot <abmcompnysn@gmail.com>
2025-11-22 23:02:36 +0000

    Message clair sur les modifications

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 /* ================================================================== */
 
 
-const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycby-mRZiij_v1JZjJThUrk6rFVAAGroZnOLKvbxEe8lkKsWkIPZ_R_paW4tBMKq3All5CQ/exec';
+const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwyE4tGKka2FpNP32PrbP0C-slakwCA4a_QZmY5yea8h_q69fZdtUAN_Fw_237b3Uln3A/exec';
 // CORRECTION: Utilisation des noms de clés de session cohérents avec les autres fichiers.
 // --- Clés de session et autres constantes ---
 const ADMIN_SESSION_KEY = 'abm_admin_token';
